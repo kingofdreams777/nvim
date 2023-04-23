29586891f1153ff7b7bc52ffed2e82591b888d09
kingofdreams777 <rossmartin@sandiego.edu>
2023-04-23 10:58:46 -0700

    removing harpoon plugin. Sorry primeagen

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,20 +10,6 @@ return {
 			require("nvim-autopairs").setup {}
 		end,
 	},
-	{"ThePrimeagen/harpoon",
-		config  = function ()
-			local mark = require("harpoon.mark")
-			local ui = require("harpoon.ui")
-
-			vim.keymap.set("n", "<leader>m", mark.add_file)
-			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-			vim.keymap.set("n", "<C-h>", function () ui.nav_file(1) end)
-			vim.keymap.set("n", "<C-t>", function () ui.nav_file(2) end)
-			vim.keymap.set("n", "<C-n>", function () ui.nav_file(3) end)
-			vim.keymap.set("n", "<C-s>", function () ui.nav_file(4) end)
-			vim.keymap.set("n", "<leader>clear", mark.clear_all)
-		end
-	},
 	{"nvim-tree/nvim-tree.lua",
 		dependencies = {"nvim-tree/nvim-web-devicons"},
 		cmd = {"NvimTreeToggle", "NvimTreeFocus"},
