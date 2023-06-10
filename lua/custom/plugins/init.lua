a1665bcb7c43adbdd7c885c59b6a745664171dd5
kingofdreams777 <rossmartin@sandiego.edu>
2023-06-10 09:59:28 -0700

    updating to use toggleterm

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,54 +4,62 @@
 -- See the kickstart.nvim README for more information
 
 return {
-	{"github/copilot.vim"},
-	{"windwp/nvim-autopairs",
-		config = function ()
-			require("nvim-autopairs").setup {}
-		end,
-	},
-	{"nvim-tree/nvim-tree.lua",
-		dependencies = {"nvim-tree/nvim-web-devicons"},
-		cmd = {"NvimTreeToggle", "NvimTreeFocus"},
-		opts = {
-			filters = {
-				dotfiles = false,
-			},
-			disable_netrw = true,
-			hijack_netrw = true,
-			hijack_cursor = true,
-			hijack_unnamed_buffer_when_opening = false,
-			sync_root_with_cwd = true,
-			update_focused_file = {
-				enable = true,
-				update_root = false,
-			},
-			view = {
-				adaptive_size = false,
-				side = "left",
-				width = 30,
-				preserve_window_proportions = true,
-			},
-			git = {
-				enable = false,
-				ignore = true
-			}
-		},
-		config = function (_, opts)
-			require("nvim-tree").setup(opts)
-			vim.g.nvimtree_side = opts.view.side
-			vim.cmd.hi("NvimTreeNormal", "guibg=NONE")
-		end,
-	},
-	{
-		"saecki/crates.nvim",
-		ft = {"rust", "toml"},
-		config = function (_, opts)
-			local crates = require("crates")
-			crates.setup(opts)
-			crates.show()
-			vim.keymap.set("n", "<leader>rcu", crates.update_all_crates)
-			vim.keymap.set("n", "<leader>rod", crates.open_documentation)
-		end,
-	},
+    { "github/copilot.vim" },
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end,
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+        opts = {
+            filters = {
+                dotfiles = false,
+            },
+            disable_netrw = true,
+            hijack_netrw = true,
+            hijack_cursor = true,
+            hijack_unnamed_buffer_when_opening = false,
+            sync_root_with_cwd = true,
+            update_focused_file = {
+                enable = true,
+                update_root = false,
+            },
+            view = {
+                adaptive_size = false,
+                side = "left",
+                width = 30,
+                preserve_window_proportions = true,
+            },
+            git = {
+                enable = false,
+                ignore = true
+            }
+        },
+        config = function(_, opts)
+            require("nvim-tree").setup(opts)
+            vim.g.nvimtree_side = opts.view.side
+            vim.cmd.hi("NvimTreeNormal", "guibg=NONE")
+        end,
+    },
+    {
+        "saecki/crates.nvim",
+        ft = { "rust", "toml" },
+        config = function(_, opts)
+            local crates = require("crates")
+            crates.setup(opts)
+            crates.show()
+            vim.keymap.set("n", "<leader>rcu", crates.update_all_crates)
+            vim.keymap.set("n", "<leader>rod", crates.open_documentation)
+        end,
+    },
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        cmd = { "ToggleTerm", "ToggleTermOpenAll", "ToggleTermCloseAll" },
+        config = true
+    }
 }
